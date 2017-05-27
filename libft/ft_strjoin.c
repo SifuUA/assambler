550a8a4eaf32af89c07988e31f7fab2f3380b757
SifuUA <alekseykres@gmail.com>
2017-05-27 11:32:51 +0300

    start

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okres <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/15 16:21:34 by okres             #+#    #+#             */
+/*   Updated: 2017/05/15 16:21:39 by okres            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (s == NULL)
+		return (NULL);
+	ft_strcat(s, s1);
+	ft_strcat(s, s2);
+	return (s);
+}
